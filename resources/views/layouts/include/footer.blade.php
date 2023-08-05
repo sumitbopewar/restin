1698d9cc2be6ad855717caac39733caaa00d4b16
KrunalTarale <krunaltarale529@gmail.com>
2023-08-05 13:59:14 +0530

    add icons in to footer

--- resources/views/layouts/include/footer.blade.php
+++ resources/views/layouts/include/footer.blade.php
@@ -1,173 +1,180 @@
 <style>
-    .information-block {
-        display: flex;
-        justify-content: space-between;
-        flex-wrap: wrap;
-    }
+.information-block {
+    display: flex;
+    justify-content: space-between;
+    flex-wrap: wrap;
+}
 
-    footer {
-        display: flex;
-        justify-content: space-between;
-        flex-wrap: wrap;
-        background: rgb(0, 26, 73);
-        background: linear-gradient(142deg, rgba(0, 26, 73, 0.9864320728291317) 0%, rgba(0, 0, 96, 1) 17%, rgba(0, 52, 255, 1) 81%);
-        color: white;
-        /* background-image: url('WhatsApp Image 2023-07-28 at 4.40.49 PM.jpeg'); */
-    }
+footer {
+    display: flex;
+    justify-content: space-between;
+    flex-wrap: wrap;
+    background: rgb(0, 26, 73);
+    background: linear-gradient(142deg, rgba(0, 26, 73, 0.9864320728291317) 0%, rgba(0, 0, 96, 1) 17%, rgba(0, 52, 255, 1) 81%);
+    color: white;
+    /* background-image: url('WhatsApp Image 2023-07-28 at 4.40.49 PM.jpeg'); */
+}
 
-    .footer-links {
-        margin: 2rem
-    }
+.footer-links {
+    margin: 2rem
+}
 
-    .copywrite {
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        margin: 2rem;
-        text-align: right;
-    }
+.copywrite {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    margin: 2rem;
+    text-align: right;
+}
 
-    .footer-links h4 {
-        font-size: 1.7rem;
-    }
+.footer-links h4 {
+    font-size: 1.7rem;
+}
 
-    ul li {
-        font-size: 1rem;
-        list-style: none;
-    }
+ul li {
+    font-size: 1rem;
+    list-style: none;
+}
 
-    /*  ul{
+/*  ul{
             padding-left: 0px;
         } */
 
-    a {
-        text-decoration: none;
-        color: white;
-        cursor: pointer;
+a {
+    text-decoration: none;
+    color: white;
+    cursor: pointer;
+}
+
+.area {
+    background: #264394;
+    background: -webkit-linear-gradient(to left, #8f94fb, #4e54c8);
+    width: 100%;
+    height: auto;
+    position: absolute;
+}
+
+.circles {
+    position: absolute;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    overflow: hidden;
+    margin: 0px;
+}
+
+.circles li {
+    position: absolute;
+    display: block;
+    list-style: none;
+    width: 20px;
+    height: 20px;
+    background: rgba(255, 255, 255, 0.2);
+    animation: animate 25s linear infinite;
+    bottom: -150px;
+
+}
+
+.circles li:nth-child(1) {
+    left: 25%;
+    width: 80px;
+    height: 80px;
+    animation-delay: 0s;
+}
+
+
+.circles li:nth-child(2) {
+    left: 10%;
+    width: 20px;
+    height: 20px;
+    animation-delay: 2s;
+    animation-duration: 12s;
+}
+
+.circles li:nth-child(3) {
+    left: 70%;
+    width: 20px;
+    height: 20px;
+    animation-delay: 4s;
+}
+
+.circles li:nth-child(4) {
+    left: 40%;
+    width: 60px;
+    height: 60px;
+    animation-delay: 0s;
+    animation-duration: 18s;
+}
+
+.circles li:nth-child(5) {
+    left: 65%;
+    width: 20px;
+    height: 20px;
+    animation-delay: 0s;
+}
+
+.circles li:nth-child(6) {
+    left: 75%;
+    width: 110px;
+    height: 110px;
+    animation-delay: 3s;
+}
+
+.circles li:nth-child(7) {
+    left: 35%;
+    width: 150px;
+    height: 150px;
+    animation-delay: 7s;
+}
+
+.circles li:nth-child(8) {
+    left: 50%;
+    width: 25px;
+    height: 25px;
+    animation-delay: 15s;
+    animation-duration: 45s;
+}
+
+.circles li:nth-child(9) {
+    left: 20%;
+    width: 15px;
+    height: 15px;
+    animation-delay: 2s;
+    animation-duration: 35s;
+}
+
+.circles li:nth-child(10) {
+    left: 85%;
+    width: 150px;
+    height: 150px;
+    animation-delay: 0s;
+    animation-duration: 11s;
+}
+
+
+
+@keyframes animate {
+
+    0% {
+        transform: translateY(0) rotate(0deg);
+        opacity: 1;
+        border-radius: 0;
     }
 
-    .area {
-        background: #264394;
-        background: -webkit-linear-gradient(to left, #8f94fb, #4e54c8);
-        width: 100%;
-        height: auto;
-        position: absolute;
+    100% {
+        transform: translateY(-1000px) rotate(720deg);
+        opacity: 0;
+        border-radius: 50%;
     }
 
-    .circles {
-        position: absolute;
-        left: 0;
-        width: 100%;
-        height: 100%;
-        overflow: hidden;
-        margin: 0px;
-    }
+}
 
-    .circles li {
-        position: absolute;
-        display: block;
-        list-style: none;
-        width: 20px;
-        height: 20px;
-        background: rgba(255, 255, 255, 0.2);
-        animation: animate 25s linear infinite;
-        bottom: -150px;
-
-    }
-
-    .circles li:nth-child(1) {
-        left: 25%;
-        width: 80px;
-        height: 80px;
-        animation-delay: 0s;
-    }
-
-
-    .circles li:nth-child(2) {
-        left: 10%;
-        width: 20px;
-        height: 20px;
-        animation-delay: 2s;
-        animation-duration: 12s;
-    }
-
-    .circles li:nth-child(3) {
-        left: 70%;
-        width: 20px;
-        height: 20px;
-        animation-delay: 4s;
-    }
-
-    .circles li:nth-child(4) {
-        left: 40%;
-        width: 60px;
-        height: 60px;
-        animation-delay: 0s;
-        animation-duration: 18s;
-    }
-
-    .circles li:nth-child(5) {
-        left: 65%;
-        width: 20px;
-        height: 20px;
-        animation-delay: 0s;
-    }
-
-    .circles li:nth-child(6) {
-        left: 75%;
-        width: 110px;
-        height: 110px;
-        animation-delay: 3s;
-    }
-
-    .circles li:nth-child(7) {
-        left: 35%;
-        width: 150px;
-        height: 150px;
-        animation-delay: 7s;
-    }
-
-    .circles li:nth-child(8) {
-        left: 50%;
-        width: 25px;
-        height: 25px;
-        animation-delay: 15s;
-        animation-duration: 45s;
-    }
-
-    .circles li:nth-child(9) {
-        left: 20%;
-        width: 15px;
-        height: 15px;
-        animation-delay: 2s;
-        animation-duration: 35s;
-    }
-
-    .circles li:nth-child(10) {
-        left: 85%;
-        width: 150px;
-        height: 150px;
-        animation-delay: 0s;
-        animation-duration: 11s;
-    }
-
-
-
-    @keyframes animate {
-
-        0% {
-            transform: translateY(0) rotate(0deg);
-            opacity: 1;
-            border-radius: 0;
+.footer_icons {
+            width: 25px;
+            height: 30px;
+            margin: 5px;
+            object-fit: scale-down;
         }
-
-        100% {
-            transform: translateY(-1000px) rotate(720deg);
-            opacity: 0;
-            border-radius: 50%;
-        }
-
-    }
 </style>
 
 
@@ -201,26 +208,29 @@
         <div class="important_links footer-links">
             <h4>Support</h4>
             <ul>
-                <li><a href="">Privacy & policy</a></li>
-                <li><a href="">Terms & conditions</a></li>
-                <li><a href="">Warrenty</a></li>
+                <li><a href=""><img class="footer_icons" src="{{ asset('layouts/img/hospital.png') }}" alt="">Privacy & policy</a></li>
+                <li><a href=""><img class="footer_icons" src="{{ asset('layouts/img/terms-and-conditions.png') }}" alt="">Terms & conditions</a></li>
+                <li><a href=""><img class="footer_icons" src="{{ asset('layouts/img/Warranty.png') }}" alt="">Warrenty</a></li>
             </ul>
         </div>
 
         <div class="Collecitons footer-links">
             <h4>Payment Methods</h4>
             <ul>
-                <li>Visa Card</li>
-                <li>Mastercard</li>
-                <li>Debit Card</li>
-                <li>Credit Card</li>
-                <li>Bank Transfers</li>
-                <li>Mobile payments</li>
+                <li> <img class="footer_icons" src="{{ asset('layouts/img/visa.png') }}" alt="">Visa Card</li>
+                <li><img class="footer_icons" src="{{ asset('layouts/img/money.png') }}" alt="">Mastercard</li>
+                <li><img class="footer_icons" src="{{ asset('layouts/img/atm-card.png') }}" alt="">Debit Card
+                </li>
+                <li><img class="footer_icons" src="{{ asset('layouts/img/credit-card (1).png') }}" alt="">Credit Card
+                </li>
+                <li><img class="footer_icons" src="{{ asset('layouts/img/provider.png') }}" alt="">Bank Transfers</li>
+                <li><img class="footer_icons" src="{{ asset('layouts/img/cashless-payment.png') }}" alt="">Mobile
+                    payments</li>
             </ul>
         </div>
     </div>
 
-    <div class="copywrite"> 
+    <div class="copywrite">
         <h3>Restin</h3>
         <p>Copyright © 2023 All rights reserved</p>
     </div>
@@ -228,7 +238,9 @@
 
 
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
-    integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
-    crossorigin="anonymous"></script>
+    integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+</script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+</script>
--- resources/views/royal.blade.php
+++ resources/views/royal.blade.php
@@ -2,74 +2,77 @@
 
 
 @section('content')
-    <style>
-        .card {
-            width: 400px;
-            margin: 20px;
-            padding: 0px;
-        }
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Lisu+Bosa:wght@200&family=Noto+Serif+Khojki:wght@500&family=Roboto&display=swap');
 
-        .card-img-top:hover {
-            position: relative;
-            float: center;
-            z-index: 2;
-            width: 400px;
-            height: auto;
-            transform: scale(1.1);
-            transition: transform 0.3s ease-in-out;
-        }
+.card {
+    width: 400px;
+    margin: 20px;
+    padding: 0px;
+}
 
-        .home_mattress_head {
-            margin-top: 6rem;
-            background-image: url('{{ asset('/layouts/img/murron bg.png') }}');
-            background-size: cover;
-            background-repeat: no-repeat;
-            /* padding: 10rem; */
-            padding: 73px;
-            color: white;
-            font-size: 3rem;
-        }
+.card-img-top:hover {
+    position: relative;
+    float: center;
+    z-index: 2;
+    width: 400px;
+    height: auto;
+    transform: scale(1.1);
+    transition: transform 0.3s ease-in-out;
+}
 
-        .royal_head{
-            color: #f3e37d;
-        }
-    </style>
+.home_mattress_head {
+    margin-top: 6rem;
+    background-image: url('{{ asset('/layouts/img/murron bg.png') }}');
+    background-size: cover;
+    background-repeat: no-repeat;
+    /* padding: 10rem; */
+    padding: 73px;
+    color: white;
+    font-size: 3rem;
+}
 
-    <!--<h1>Royal Mattress</h1>-->
-    <div class=" text-center home_mattress_head d-grid justify-content-center">
-        <img src="{{ asset('layouts/img/RR Logo.png') }}"alt="" style="width: 200px; object-fit: contain;">
-        <h2 class="royal_head">Royal Mattress</h2>
+.royal_head {
+    color: #f3e37d;
+    font-family: 'Noto Serif Khojki', serif !important;
+}
+</style>
+
+<!--<h1>Royal Mattress</h1>-->
+<div class=" text-center home_mattress_head d-grid justify-content-center">
+    <img src="{{ asset('layouts/img/RR Logo.png') }}" alt="" style="width: 200px; object-fit: contain;">
+    <h2 class="royal_head">Royal Mattress</h2>
+</div>
+
+<section class="mx-3">
+    <div class="row d-flex justify-content-center align-items-center cart-container">
+
+        @foreach ($royal as $item)
+        <div class="_card col-lg-4 col-md-6">
+            <div class="royal_cart-img card-img">
+                <a href="#"> <img src="{{ asset('layouts/img/'.$item->image) }}" class="product_img" alt="" srcset="">
+                </a>
+            </div>
+            <div class="card-info">
+                <p class="text-title product_name">{{$item->product}}</p>
+            </div>
+            <div class="_card-footer">
+                <span class="text-title _product_price"></span>
+                <div class="royal_card-button _card-button">
+                    <a href="#"><i class="bi bi-cart3 cart_logo"></i></a>
+                </div>
+            </div>
+        </div>
+        @endforeach
+
+
+
     </div>
 
-    <section class="mx-3">
-        <div class="row d-flex justify-content-center align-items-center cart-container">
 
-            @foreach ($royal as $item)
-                <div class="_card col-lg-4 col-md-6">
-                    <div class="royal_cart-img card-img">
-                        <a href="#"> <img src="{{ asset('layouts/img/'.$item->image) }}" class="product_img"
-                                alt="" srcset=""> </a>
-                    </div>
-                    <div class="card-info">
-                        <p class="text-title product_name">{{$item->product}}</p>
-                    </div>
-                    <div class="_card-footer">
-                        <span class="text-title _product_price"></span>
-                        <div class="royal_card-button _card-button">
-                            <a href="#"><i class="bi bi-cart3 cart_logo"></i></a>
-                        </div>
-                    </div>
-                </div>
-            @endforeach
+    </div>
+</section>
 
 
-
-        </div>
-
-
-        </div>
-    </section>
-
-
-    <!-- Carts Ends -->
+<!-- Carts Ends -->
 @endsection
