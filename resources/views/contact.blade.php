dcb186ed5fe1f1db10224fde7addea7e184ca2b1
nikhil08 <nmarbate99@gmail.com>
2023-08-09 16:49:52 +0530

    add design in css

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -51,42 +51,12 @@
             An Email. Our Staff Always BE there To Help You Out. Do Not Hesitate!</p>
 </div>
 <div class="container pb-2">
-    <div class="row d-flex justify-content-center align-items-center">
+    <div class="row d-flex justify-content-center align-items-center mb-4">
 
-        <div class="col-12 col-sm-12 col-md-12 col-lg-6  my-4">
-            <div class="shadow p-4" style="min-height:100%;">
-                <h6 class="fw-bold text-center">Contact Info</h6>
-                <!-- <div class="py-2"><i class="fa-solid fa-phone px-2"></i>Mobile No.:<span class="px-5">+91
-                        7755918227</span></div>
-                <div class="py-2"><i class="fa-solid fa-envelope px-2"></i>Email:<span
-                        class="px-5">sales@restin.in</span></div>
-                <div class="py-2"><i class="fa-solid fa-location-dot px-2"></i>Address:<span class="px-5">Restin
-                        Industries N 101/7 MIDC Hingna Road Nagpur 440016 Maharashtra, India</span></div> -->
+        <div class="col-12 col-sm-12 col-md-12 col-lg-6 shadow  p-0">
 
-                   <style>
-                        .address_head{
-                            display: flex;
-                            flex-direction: row;
-                        }
-                   </style>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3722.2135110700037!2d78.98993407479814!3d21.10405228518917!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bd495370eab2b61%3A0xc3d02fda1e4fceb!2sRestin%20Industries!5e0!3m2!1sen!2sin!4v1691574861221!5m2!1sen!2sin" width="100%" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
 
-                <div class=details>
-                    <p class="detail-head address_head"><i class="fa-solid fa-location-dot px-2"></i><span>Address:</span></p>
-                    <p class="detail">Restin Industries N 101/7 MIDC Hingna Road Nagpur 440016 Maharashtra, India</p>
-                </div>
-
-                <div class=details>
-                    <p class="detail-head"><i class="fa-solid fa-phone px-2"></i>Mobile No.:</p>
-                    <p class="detail">+91 7755918227</p>
-                </div>
-
-                <div class=details>
-                    <p class="detail-head"><i class="fa-solid fa-envelope px-2"></i>Email:</p>
-                    <p class="detail">sales@restin.in</p>
-                </div>
-
-
-            </div>
         </div>
 
         <div class="col-12 col-sm-12 col-md-12 col-lg-6 ">
@@ -100,13 +70,11 @@
                             <input type="email" placeholder="Enter Email" class="bg border-0 ps-3 py-2 rounded-0 w-100">
                         </div>
                         <div class="col-6 my-2">
-                            <input type="number" placeholder="Enter Mobile No."
-                                class="bg border-0 ps-3 py-2 rounded-0 w-100">
+                            <input type="number" placeholder="Enter Mobile No." class="bg border-0 ps-3 py-2 rounded-0 w-100">
                         </div>
                     </div>
                     <div class="my-2">
-                        <textarea class="bg border-0 ps-3 py-2 rounded-0 w-100 " placeholder="Comments"
-                            rows="5"></textarea>
+                        <textarea class="bg border-0 ps-3 py-2 rounded-0 w-100 " placeholder="Comments" rows="5"></textarea>
                     </div>
                     <div class="my-2">
                         <input type="submit" value="Submit" class="btn btn-info w-100 rounded-0">
--- resources/views/guide_to_good_sleep.blade.php
+++ resources/views/guide_to_good_sleep.blade.php
@@ -10,20 +10,20 @@
 <div class="container p2 page2">
     <div>
         <h4>BENEFITS OF GETTING GOOD SLEEP</h4>
-        <p>When we sleep, our body restore, rejuvenate, and regenerate it is a time to recover from the previous day
+        <p style="text-align: justify">When we sleep, our body restore, rejuvenate, and regenerate it is a time to recover from the previous day
             while repairing and preparing for the next. Adequate sleep reduces stress and illness, balances our
             emotions, and improves our overall well being. Sleep helps us conserve our energy resources to ensure
             maximum performance throughout the day.
         </p>
     </div>
 
-    <div class="parent-content row">
+    <div class="parent-content row" style="margin-top: 30px;">
 
-        <div class="sub-content col-lg-6">
+        <div class="sub-content col-lg-6" style="margin-top: 25px;">
 
             <div class="mb-3">
                 <h4>BENEFITS OF GETTING GOOD SLEEP</h4>
-                <p>When we sleep, our body restore, rejuvenate, and regenerate. It is a time to recover from the
+                <p style="text-align: justify">When we sleep, our body restore, rejuvenate, and regenerate. It is a time to recover from the
                     previous day while repairing and preparing for the next. Adequate sleep reduces stress and illness,
                     balances our emotions, and improves our overall well being. Sleep helps us conserve our energy
                     resources to ensure maximum performance throughout the day.
@@ -32,7 +32,7 @@
 
             <div>
                 <h4>HEALTH BENEFITS OF A QUALITY MATTRESS </h4>
-                <p>When it comes to getting adequate sleep, six hours of uninterrupted sleep is better than eight or
+                <p style="text-align: justify">When it comes to getting adequate sleep, six hours of uninterrupted sleep is better than eight or
                     nine of fragmented sleep. Research shows that a mattress can either rob you of sleep, or encourage
                     it. If it's doing its job, it will reduce tossing and turning, provide proper spinal support,
                     improve circulation, and cushion the body's pressure points. A comfortable, supportive mattress can
@@ -45,19 +45,19 @@
 
 
         <div class="p col-lg-6" style="margin: auto;">
-            <img class="para-img img-fluid" src="{{asset('layouts/img/Para-img.jpg')}}" alt="" style="padding: 0px;">
+            <img class="para-img img-fluid" src="{{asset('layouts/img/Para-img.jpg')}}" alt="" style="padding: 0px; margin:20px; height:360px; width:100%">
         </div>
 
     </div>
 </div>
 
 <!-- Paragraph Section 1 -->
-<div class="container md-5 mb-3">
+<div class="container md-5 mb-3" style="margin-top: 30px; ">
     <div class="">
         <div class="">
 
             <h4>HOW DO YOU KNOW WHEN IT'S TIME FOR A NEW MATTRESS ?</h4>
-            <p>Don't look at your warranty to tell you how long to keep your mattress. The warranty on a mattress is not
+            <p style="text-align: justify">Don't look at your warranty to tell you how long to keep your mattress. The warranty on a mattress is not
                 an indicator of how long the product should be used before replacement. Normally, a mattress provides
                 optimum service for about six to eight years of nightly use. If your mattress is no longer comfortable
                 or supportive, and you're waking up tired, stiff, cranky, and with aching muscles and joints, it could
