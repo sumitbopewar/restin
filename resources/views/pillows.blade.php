ff679315a38263ad725fe4fb9bfa64d95d780286
nikhil08 <nmarbate99@gmail.com>
2023-08-10 15:28:56 +0530

    royal change

--- resources/views/pillows.blade.php
+++ resources/views/pillows.blade.php
@@ -30,12 +30,13 @@
     .pillow_img {
         object-fit: scale-down;
         /* width: 20%; */
-        margin: auto;
+        /* margin: auto; */
     }
 
     .content_box {
         justify-content: space-around;
-        padding: 15px;
+        /* padding: 15px; */
+        margin-top: 20px;
 
     }
 
@@ -46,13 +47,23 @@
 
     .product_content {
         width: 50%;
-        padding: 10px;
+        /* padding: 10px; */
+        padding-right: 20px;
     }
 
     .pillow_img {
         width: 50%;
-        padding: 10px;
+        /* padding: 10px; */
     }
+
+    .main-box{
+        padding: 15px;margin-top: 20px;
+    }
+
+    .product_content li{
+        margin: 10px; text-align: justify;
+    }
+
 
     @media screen and (max-width: 1000px) {
         .content_box {
@@ -67,6 +78,7 @@
             width: 100%;
         }
     }
+    
 </style>
 
 <!--<h1>Home Mattress</h1>-->
@@ -81,33 +93,42 @@
     </p>
 
 
-
-    <div class="d-flex content_box ">
-        <div class="product_content" style="align-self: center;">
-            <h3 style="margin-bottom:10px;">Fiber Pillows</h3>
-            <p style="text-align: justify">Pillows are filled with hypoallergenic polyester fibers-Polyester is a synthetic fabric engineered to mimic the softness, coolness, and breathability of natural cotton. They come in a wide range of sizes, as well as loft and firmness settings. It provides good support, is light weight, and spot washable.</p>
-        </div>
-        <div class="pillow_img">
-            <img src="{{asset('layouts/img/pillow1.png')}}" class="product_img" alt="Pillow img" style="height: 300px; width: 560px;">
-
-
+    <div class="main-box bg-light" >
+        <h3 class="text-center">Fiber Pillows</h3>
+        <div class="d-flex content_box ">
+            <div class="product_content" style="align-self: center;">
+                <p style="text-align: justify">Pillows are filled with hypoallergenic polyester fibers-Polyester is a synthetic fabric engineered to mimic the softness, coolness, and breathability of natural cotton. They come in a wide range of sizes, as well as loft and firmness settings. It provides good support, is light weight, and spot washable.</p>
+            </div>
+            <div class="pillow_img">
+                <img src="{{asset('layouts/img/pillow1.png')}}" class="product_img p-0 img-fluid" alt="Pillow img" style="height: 300px; width: 560px;">
+            </div>
         </div>
     </div>
-    <div class="d-flex content_box ">
-        <div class="product_content">
-            <h3>MEMORY FOAM PILLOWS</h3>
-            <ul>
-                <li style="margin: 10px"><b>Prevents cricks in your neck-</b> The dense material of a memory foam pillow prevents your neck from bending in awkward directions. This is why they are great to use when traveling.</li>
-                <li style="margin: 10px"><b>Keeps your spine aligned-</b> As the memory foam pillow prevents your neck from bending, it allows your spine to remain aligned.</li>
-                <li style="margin: 10px"><b>Molds to the shape of your head –</b> Memory foam pillows contour to the shape of your head, just as a memory foam mattress contours itself to your body.</li>
-                <li style="margin: 10px">Memory foam pillows are available in a variety of shapes, which is convenient because the type of pillow needed will vary based upon your sleeping position.</li>
-                <li style="margin: 10px"><b>No adjusting-</b> Memory foam does not require flipping, fluffing, or any of the adjustments of a regular pillow, as it will retain its shape and size.</li>
-                <li style="margin: 10px"><b>Reduces snoring-</b> Regular pillows tilt your head upwards, causing air passages to close, However, memory foam pillows contour to your neck, allowing your air passages to remain open.</li>
-            </ul>
+
+
+    <div class="main-box bg-light" >
+        <h3 class="text-center">MEMORY FOAM PILLOWS</h3>
+        <div class="d-flex content_box ">
+            <div class="product_content">
+                <ul>
+                    <li><b>Prevents cricks in your neck-</b> The dense material of a memory foam pillow prevents your neck from bending in awkward directions. This is why they are great to use when traveling.</li>
+                    <li><b>Keeps your spine aligned-</b> As the memory foam pillow prevents your neck from bending, it allows your spine to remain aligned.</li>
+                    <li><b>Molds to the shape of your head –</b> Memory foam pillows contour to the shape of your head, just as a memory foam mattress contours itself to your body.</li>
+                    
+                </ul>
+            </div>
+            <div class="pillow_img">
+                <img src="{{asset('layouts/img/pillow.jpeg')}}" class="product_img" alt="Pillow img">
+            </div>
+           
         </div>
-        <div class="pillow_img">
-            <img src="{{asset('layouts/img/pillow.jpeg')}}" class="product_img" alt="Pillow img">
-        </div>
+        <div>
+                <ul>
+                <li style="margin-left: 10px;">Memory foam pillows are available in a variety of shapes, which is convenient because the type of pillow needed will vary based upon your sleeping position.</li>
+                    <li style="margin: 10px; text-align: justify;"><b>No adjusting-</b> Memory foam does not require flipping, fluffing, or any of the adjustments of a regular pillow, as it will retain its shape and size.</li>
+                    <li style="margin: 10px; text-align: justify;"><b>Reduces snoring-</b> Regular pillows tilt your head upwards, causing air passages to close, However, memory foam pillows contour to your neck, allowing your air passages to remain open.</li>
+                </ul>
+            </div>
     </div>
 </div>
 
--- resources/views/royal.blade.php
+++ resources/views/royal.blade.php
@@ -3,39 +3,39 @@
 
 @section('content')
 <style>
-@import url('https://fonts.googleapis.com/css2?family=Lisu+Bosa:wght@200&family=Noto+Serif+Khojki:wght@500&family=Roboto&display=swap');
+    @import url('https://fonts.googleapis.com/css2?family=Lisu+Bosa:wght@200&family=Noto+Serif+Khojki:wght@500&family=Roboto&display=swap');
 
-.card {
-    width: 400px;
-    margin: 20px;
-    padding: 0px;
-}
+    .card {
+        width: 400px;
+        margin: 20px;
+        padding: 0px;
+    }
 
-.card-img-top:hover {
-    position: relative;
-    float: center;
-    z-index: 2;
-    width: 400px;
-    height: auto;
-    transform: scale(1.1);
-    transition: transform 0.3s ease-in-out;
-}
+    .card-img-top:hover {
+        position: relative;
+        float: center;
+        z-index: 2;
+        width: 400px;
+        height: auto;
+        transform: scale(1.1);
+        transition: transform 0.3s ease-in-out;
+    }
 
-.home_mattress_head {
-    margin-top: 4rem;
-    background-image: url('{{ asset('/layouts/img/murron bg.png') }}');
-    background-size: cover;
-    background-repeat: no-repeat;
-    /* padding: 10rem; */
-    padding: 73px;
-    color: white;
-    font-size: 3rem;
-}
+    .home_mattress_head {
+        margin-top: 4rem;
+        background-image: url('{{ asset(' /layouts/img/murron bg.png') }}');
+        background-size: cover;
+        background-repeat: no-repeat;
+        /* padding: 10rem; */
+        padding: 73px;
+        color: white;
+        font-size: 3rem;
+    }
 
-.royal_head {
-    color: #f3e37d;
-    font-family: 'Noto Serif Khojki', serif !important;
-}
+    .royal_head {
+        color: #f3e37d;
+        font-family: 'Noto Serif Khojki', serif !important;
+    }
 </style>
 
 <!--<h1>Royal Mattress</h1>-->
@@ -44,11 +44,40 @@
     <h2 class="royal_head">Royal Mattress</h2>
 </div>
 
+<div class="container">
+    <div class="mb-5">
+    <h3 class="m-0" style="font-weight:bold;">Restine Introduce: The Royal Series Mattresses</h3>
+    <h5>Restine introduces Royal reries of luxury mattresses because you should enjoy the necessary luxury of a sumptuous, sensuously comfortable bed at home.
+       <br> We also customise mattress based on your individual requirements and preferences Now Losing sleep needen't be anything to lose sleep over.
+    </h5>
+    </div>
+
+    <div>
+        <h5 class="m-0" style="font-weight:bold;">Zero Sinking:</h5>
+        <p>The structure of the mattress is designed to ensure proper body support while sleeping ensureing that you have Zero Sinking feeling even after prolonged use.</p>
+    </div>
+
+    <div>
+        <h5 class="m-0" style="font-weight:bold;">Zero Gap:</h5>
+        <p>The structured layers of different components are assembled is a manner to prevent gap in the mattress and ensuring proper support to in a manner all the curves in the body.</p>
+    </div>
+
+    <div>
+        <h5 class="m-0" style="font-weight:bold;">Zero Noise:</h5>
+        <p>The mattress are layered in a proper designed manner to ensure zero noice caused by rubbing of the inner material together.</p>
+    </div>
+
+    <div>
+        <h5 class="m-0" style="font-weight:bold;">Zero Motion:</h5>
+         <p>The inner springs in the mattress are independent of each other - they offrindividual support, providing greater comfort, so there is less tossing and turning there by eliminationg the discomfort to the other sleeping partner.</p>
+    </div>
+</div>
+
 <section class="mx-3">
     <div class="row d-flex justify-content-center align-items-center cart-container">
 
         @foreach ($royal as $item)
-        <div class="_card col-lg-4 col-md-6">
+        <div class="_card col-lg-3 col-md-6">
             <div class="royal_cart-img card-img">
                 <a href="#"> <img src="{{ asset('layouts/img/'.$item->image) }}" class="product_img" alt="" srcset="">
                 </a>
