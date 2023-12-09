6d0d25d2e39f3456e8d2fe245c6113a3f1607faa
bawanesumit01 <bawanesumit01@gmail.com>
2023-12-09 14:51:04 +0530

    14:50 changes

--- resources/views/layouts/include/navbar.blade.php
+++ resources/views/layouts/include/navbar.blade.php
@@ -130,7 +130,8 @@
                             <li class="nav-item dropdown text-center wow bounceInUp" data-wow-delay="1.0s">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
                                     data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }}
+                                    <img
+                                            src="{{ asset('assets_new/images/user.svg') }}">
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
